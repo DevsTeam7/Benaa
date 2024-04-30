5263250b1a3f973d095df48abad310cf5cb0a1f1
Yazed Eraqi <Yazedir45@gmail.com>
2024-04-30 07:13:07 +0300

    adding dashboard

---
+++ Benaa.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Benaa.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
